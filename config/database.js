f34a875f874466f884aa3795f261de47510fb270
Boonchai Wedwanitsatheiin <neng87@gmail.com>
2021-12-10 08:11:06 +0700

    use env variable instead plain mongodb uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://thaijavadev:ThaiJavaDev123@cluster0-rdast.mongodb.net/test?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGO_DATABASE_URL;
 mongoose.connect(mongoDB,{ useNewUrlParser: true,useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
