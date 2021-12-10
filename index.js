ba1c2fde63f89145fdd5da77f0692e5d22693471
Boonchai Wedwanitsatheiin <neng87@gmail.com>
2021-12-10 18:30:55 +0700

    remove bodyParser.urlencode

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
     res.json({ "tutorial": "Build REST API with node.js" });
 });
